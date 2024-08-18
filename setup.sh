25197681d5cb3c261c12e3ba30cf88910a5bec07
zlin888 <45032494+zlin888@users.noreply.github.com>
2024-08-17 20:15:58 -0400

    add setup

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ sudo apt-get install silversearcher-ag
+ sudo apt install ctags
+ 
+ pip install jedi-language-server
